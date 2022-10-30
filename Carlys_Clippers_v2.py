fd6a428ec41d947c4b7fe6b245089feab60562bc
jleepee <113560007+jleepee@users.noreply.github.com>
2022-10-30 15:22:50 -0500

    Create Carlys_Clippers_v2.py

---
+++ Carlys_Clippers_v2.py
@@ -0,0 +1,37 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+total_price = 0
+
+#Find Total Price of all Haircuts
+for i in prices:
+  total_price += i
+print("Total Price for All Haircuts:", total_price)
+
+#Fine average price of haircuts
+average_price = total_price / len(prices)
+print("Average Price:", average_price)
+print("Number of haricuts:", len(hairstyles))
+
+#Reduce all prices by $5
+new_prices = [i-5 for i in prices]
+new_prices.sort()
+print("New Prices:", new_prices)
+
+#Find Total Revenue for the week
+total_revenue = 0
+
+#Loop through hairstyles creating a list with range same as hairstyles length [0,8]
+#print(range(len(hairstyles)))
+for i in range(len(hairstyles)):
+#Multiply price by # of haircuts for each index [0,8]
+  total_revenue += prices[i] * last_week[i]
+print("Total Revenue Last Week:", total_revenue)
+
+#Find Average daily revenue
+average_daily_revenue = total_revenue / 7
+print("Average Daily Revenue:", average_daily_revenue)
+
+#Advertise haircuts under $30
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if prices[i] <= 30]
+print(cuts_under_30)
